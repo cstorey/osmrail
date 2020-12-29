e9e2a311a34cb34d65c6e796c04184716ff7603a
Ceri Storey <cez@necrofish.org.uk>
2020-12-29 17:10:04 +0000

    Sorta-kinda derive station links.

--- src/bin/diffusion.rs
+++ src/bin/diffusion.rs
@@ -1,13 +1,16 @@
 use std::{
-    collections::{BTreeMap, HashMap, VecDeque},
+    collections::{BTreeMap, HashMap, HashSet, VecDeque},
     io::{Read, Seek},
     path::PathBuf,
 };
 
 use anyhow::{Context, Result};
 use im::Vector;
-use osmpbfreader::{Node, NodeId, OsmId, OsmObj, OsmPbfReader, Relation, Tags, Way};
-use petgraph::graph::{NodeIndex, UnGraph};
+use osmpbfreader::{Node, NodeId, OsmId, OsmObj, OsmPbfReader, Relation, Way};
+use petgraph::{
+    graph::{NodeIndex, UnGraph},
+    visit::EdgeRef,
+};
 use smartstring::alias::String;
 use structopt::StructOpt;
 
@@ -18,9 +21,9 @@ struct Args {
 
 #[derive(Clone, Debug, Default)]
 struct Map {
-    graph: UnGraph<OsmId, Option<String>>,
-    vertex_by_osm_id: BTreeMap<OsmId, NodeIndex>,
-    osm_id_by_vertex: BTreeMap<NodeIndex, OsmId>,
+    graph: UnGraph<NodeId, OsmId>,
+    vertex_by_osm_id: BTreeMap<NodeId, NodeIndex>,
+    osm_id_by_vertex: BTreeMap<NodeIndex, NodeId>,
     objs: BTreeMap<OsmId, OsmObj>,
     node_id_by_crs: BTreeMap<String, NodeId>,
 }
@@ -35,10 +38,11 @@ fn main() -> Result<()> {
 
     let map = Map::from_reader(&mut pbf)?;
 
-    let crses = vec!["HYS", "WWI", "EDN", "ELE", "LSY", "CFB"]
+    let _crses = vec!["HYS", "WWI", "EDN", "ELE", "LSY", "CFB"]
         .into_iter()
         .map(String::from)
         .collect::<Vec<_>>();
+    let crses = map.node_id_by_crs.keys().cloned().collect::<HashSet<_>>();
 
     println!("---");
 
@@ -51,11 +55,7 @@ fn main() -> Result<()> {
 
     for crs in crses.iter().cloned() {
         let node_id = map.node_id_by_crs.get(&crs).cloned().expect(&crs);
-        let idx = map
-            .vertex_by_osm_id
-            .get(&node_id.into())
-            .cloned()
-            .expect("vertex");
+        let idx = map.vertex_by_osm_id.get(&node_id).cloned().expect("vertex");
         let path = Vector::unit(node_id.into());
         seen.insert(idx, (crs.clone(), path.clone()));
         stack.push_back((crs, path, idx));
@@ -70,11 +70,14 @@ fn main() -> Result<()> {
             map.id_by_idx(idx),
             map.obj_by_idx(idx)
         );
-        for succ in map.graph.neighbors(idx) {
+        for succ_ref in map.graph.edges(idx) {
+            assert_eq!(succ_ref.source(), idx);
+            let succ = succ_ref.target();
+
             let succ_osm_id = map.osm_id_by_vertex.get(&succ).cloned().expect("osm_id");
             match seen.get(&succ) {
                 Some((succ_crs, succ_path)) if crs != *succ_crs => {
-                    println!("Boundary:\t{}[{:?}]—{}[{:?}]", crs, idx, succ_crs, succ);
+                    println!("Boundary:\t{}[{:?}]--{}[{:?}]", crs, idx, succ_crs, succ);
                     let key;
                     let full_path;
                     if &crs <= succ_crs {
@@ -101,7 +104,9 @@ fn main() -> Result<()> {
                 None => {
                     println!("New:\t{}[{:?}]", crs, succ);
                     let mut path = path.clone();
-                    path.push_back(succ_osm_id);
+
+                    path.push_back(*succ_ref.weight());
+                    path.push_back(succ_osm_id.into());
 
                     seen.insert(succ, (crs.clone(), path.clone()));
                     stack.push_back((crs.clone(), path, succ));
@@ -112,10 +117,11 @@ fn main() -> Result<()> {
     println!("---");
 
     for ((a, b), path) in boundaries {
-        println!("{}—{}", a, b);
-        for osm_id in path.iter() {
-            println!("\t{:?}: {:?}", osm_id, map.objs.get(osm_id));
-        }
+        // println!("{}--{}", a, b);
+        // for osm_id in path.iter() {
+        //     println!("\t{:?}: {:?}", osm_id, map.objs.get(osm_id));
+        // }
+        println!("{}--{}; {:?}", a, b, path);
     }
 
     Ok(())
@@ -125,18 +131,12 @@ impl Map {
     fn from_reader<R: Read + Seek>(pbf: &mut OsmPbfReader<R>) -> Result<Self> {
         let mut map = Map::default();
 
-        fn is_relevant(tags: &Tags) -> bool {
-            tags.contains_key("railway") || tags.contains_key("public_transport")
-        }
-
         for it in pbf.iter() {
             let it = it.context("Read item")?;
-            if is_relevant(it.tags()) {
-                match it {
-                    OsmObj::Node(n) => map.add_node(n),
-                    OsmObj::Way(w) => map.add_way(w),
-                    OsmObj::Relation(r) => map.add_rel(r),
-                }
+            match it {
+                OsmObj::Node(n) => map.add_node(n),
+                OsmObj::Way(w) => map.add_way(w),
+                OsmObj::Relation(r) => map.add_rel(r),
             }
         }
 
@@ -145,14 +145,19 @@ impl Map {
 
     fn obj_by_idx(&self, idx: NodeIndex) -> Option<OsmObj> {
         self.id_by_idx(idx)
-            .and_then(|osm_id| self.objs.get(&osm_id).cloned())
+            .and_then(|osm_id| self.objs.get(&osm_id.into()).cloned())
     }
 
-    fn id_by_idx(&self, idx: NodeIndex) -> Option<OsmId> {
+    fn id_by_idx(&self, idx: NodeIndex) -> Option<NodeId> {
         self.osm_id_by_vertex.get(&idx).cloned()
     }
 
+    // We mostly just want railway=station here.
     fn add_node(&mut self, node: Node) {
+        if !(node.tags.contains_key("railway") || node.tags.contains_key("public_transport")) {
+            return;
+        }
+
         let vid = self.index(node.id);
         println!(
             "{:?}[{:?}]: {:?}, {},{}",
@@ -168,54 +173,52 @@ impl Map {
         self.objs.insert(node.id.into(), node.into());
     }
 
+    // "We mostly just care about routes here."
     fn add_way(&mut self, w: Way) {
-        let way_idx = self.index(w.id);
-        println!(
-            "{:?}[{:?}]: {:?}; {:?}",
-            OsmId::from(w.id),
-            way_idx,
-            w.tags,
-            w.nodes
-        );
+        if !(w.tags.contains_key("railway") || w.tags.contains_key("public_transport")) {
+            return;
+        }
 
-        for node_id in w.nodes.iter().cloned() {
-            println!("{:?} -- {:?}", OsmId::from(w.id), OsmId::from(node_id));
-            let n_idx = self.index(node_id);
-            self.graph.add_edge(way_idx, n_idx, None);
+        println!("{:?}: {:?}; {:?}", OsmId::from(w.id), w.tags, w.nodes);
+
+        for (a, b) in w.nodes.iter().cloned().zip(w.nodes.iter().skip(1).cloned()) {
+            let a_idx = self.index(a);
+            let b_idx = self.index(b);
+            println!("\t{:?}[{:?}] -- {:?}[{:?}]", a, a_idx, b, b_idx);
+            self.graph.add_edge(a_idx, b_idx, w.id.into());
         }
         self.objs.insert(w.id.into(), w.into());
     }
 
     fn add_rel(&mut self, r: Relation) {
-        let rel_idx = self.index(r.id);
-        println!(
-            "{:?}[{:?}]: {:?}; {:?}",
-            OsmId::from(r.id),
-            rel_idx,
-            r.tags,
-            r.refs
-        );
+        if r.tags.get("public_transport").map(|s| &**s) != Some("stop_area") {
+            return;
+        }
 
-        for it in r.refs.iter() {
-            println!("{:?} -- {:?}[{}]", OsmId::from(r.id), it.member, it.role);
-            let member_idx = self.index(it.member);
-            self.graph
-                .add_edge(rel_idx, member_idx, Some(it.role.clone()));
+        println!("{:?}: {:?}; {:?}", OsmId::from(r.id), r.tags, r.refs);
+
+        let nodes = r.refs.iter().flat_map(|r| r.member.node());
+        for a in nodes.clone() {
+            let a_idx = self.index(a);
+            for b in nodes.clone().filter(|&b| a < b) {
+                let b_idx = self.index(b);
+                println!("\t{:?}[{:?}] -- {:?}[{:?}]", a, a_idx, b, b_idx);
+                self.graph.add_edge(a_idx, b_idx, r.id.into());
+            }
         }
         self.objs.insert(r.id.into(), r.into());
     }
 
-    fn index(&mut self, osm_id: impl Into<OsmId>) -> NodeIndex {
-        let osm_id = osm_id.into();
+    fn index(&mut self, node_id: NodeId) -> NodeIndex {
         let Self {
             graph,
             vertex_by_osm_id,
             osm_id_by_vertex,
             ..
         } = self;
-        *vertex_by_osm_id.entry(osm_id).or_insert_with(|| {
-            let idx = graph.add_node(osm_id);
-            osm_id_by_vertex.insert(idx, osm_id);
+        *vertex_by_osm_id.entry(node_id).or_insert_with(|| {
+            let idx = graph.add_node(node_id);
+            osm_id_by_vertex.insert(idx, node_id);
             idx
         })
     }
